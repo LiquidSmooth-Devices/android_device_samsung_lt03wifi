7ee59b274c54915a9c23d1e702c22f03666e01ec
Tony Malagisi <mtb3000gt@gmail.com>
2015-05-15 21:33:05 -0400

    Bring inline with eos
    Change-Id: I07aa3c28996505ea3a7eb65731486ac6ccfdba48

--- full_lt03wifi.mk
+++ full_lt03wifi.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from lt03wifi device
 $(call inherit-product, device/samsung/lt03wifi/device.mk)
 
+# Inherit from LiquidSmooth vendor
+$(call inherit-product, vendor/liquid/config/common_full_tablet_wifionly.mk)
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_lt03wifi
 PRODUCT_DEVICE := lt03wifi
